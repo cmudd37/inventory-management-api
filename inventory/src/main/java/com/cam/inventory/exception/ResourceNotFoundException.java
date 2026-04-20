119be92d02565499bd64bc9d0b1adf643e8add6a
Cameron Mudd <cmudd37@gmail.com>
2026-04-20 16:17:59 -0400

    Fix project structure (remove nested folder)

--- inventory/src/main/java/com/cam/inventory/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.cam.inventory.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
--- inventory/src/main/java/com/cam/inventory/repository/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cam.inventory.repository;
-
-import com.cam.inventory.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface CategoryRepository extends JpaRepository<Category,Long> {
-}
