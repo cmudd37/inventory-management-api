983c540a20b28b3941f9168459247d213491618e
Cameron Mudd <cmudd37@gmail.com>
2026-05-04 12:18:21 -0400

    Updating JWT filter

--- src/main/java/com/cam/inventory/security/JwtFilter.java
+++ src/main/java/com/cam/inventory/security/JwtFilter.java
@@ -29,6 +29,11 @@ public class JwtFilter extends OncePerRequestFilter {
                                     FilterChain filterChain)
             throws ServletException, IOException {
 
+        if (request.getServletPath().startsWith("/auth")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
         String authHeader = request.getHeader("Authorization");
 
         if (authHeader != null && authHeader.startsWith("Bearer ")) {
